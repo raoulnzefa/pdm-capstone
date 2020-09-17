83b00dd743b8b3356d5ccb3e06871d2b99ba62a1
ivanpaghubasan <paghubasan.ivan@gmail.com>
2020-09-17 21:36:08 +0800

    update admin login page

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Admin Login - Infinity Fightgear</title>
-    <link rel="icon" type="image/png" href="{{ asset('images/Logo.png') }}">
+    <link rel="icon" type="image/png" href="images/Logo.png">
     <!-- Fonts -->
    {{--  <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
@@ -17,24 +17,24 @@
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     
-    <link href="{{ asset('css/my-admin.css') }}" rel="stylesheet" type="text/css">
+    <link href="css/my-admin.css" rel="stylesheet" type="text/css">
     <!-- Bootstrap core CSS-->
-    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- Custom styles for this template-->
 <body class="bg-dark">
     <div class="container" id="app">
         <div class="mt-3">
-            <img src="{{ asset('images/logo.jpg')}}" class="img-fluid d-block mr-auto ml-auto" width="300" height="300">
+            <img src="images/logo.jpg" class="img-fluid d-block mr-auto ml-auto" width="300" height="300">
         </div>
         @yield('content')
     </div>
     <!-- Scripts -->
     <!-- Bootstrap core JavaScript -->
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- Core plugin JavaScript-->
-    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 </body>
 </html>
