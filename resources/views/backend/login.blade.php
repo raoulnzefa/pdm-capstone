45d32d51d2db81aa6cbb3a10069f17b3d2e28720
ivanpaghubasan <paghubasan.ivan@gmail.com>
2020-09-17 21:40:12 +0800

    update admin login page 2

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -17,24 +17,24 @@
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     
-    <link href="css/my-admin.css" rel="stylesheet" type="text/css">
+    <link href="/css/my-admin.css" rel="stylesheet" type="text/css">
     <!-- Bootstrap core CSS-->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom fonts for this template-->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- Custom styles for this template-->
 <body class="bg-dark">
     <div class="container" id="app">
         <div class="mt-3">
-            <img src="images/logo.jpg" class="img-fluid d-block mr-auto ml-auto" width="300" height="300">
+            <img src="/images/logo.jpg" class="img-fluid d-block mr-auto ml-auto" width="300" height="300">
         </div>
         @yield('content')
     </div>
     <!-- Scripts -->
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
 </body>
 </html>
